94e8b884bc1fff6d9b227cdc04228026b17c2df9
oravital7 <oravital7@gmail.com>
2019-01-02 18:47:56 +0200

    Update Graph

--- Task4/src/AutoAlgo/CalcNeighbor.java
+++ Task4/src/AutoAlgo/CalcNeighbor.java
@@ -1,37 +1,63 @@
 package AutoAlgo;
 
 import java.awt.Rectangle;
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import Gameboard.Blocks;
 import Geom.Point3D;
 
 public class CalcNeighbor {
-	
+
 	private Vertex source, target;
 	private	ArrayList<Blocks> blockArr;
-	private ArrayList<Point3D> points;
-	
+	private ArrayList<Vertex> points;
+	private int id;
+	private HashMap<String, Point3D> hash;
+
 	public CalcNeighbor(ArrayList<Blocks> blockArr, Vertex source, Vertex target) {
 		this.source = source;
 		this.target = target;
 		this.blockArr = blockArr;
-		points = new ArrayList<Point3D>();
+		points = new ArrayList<Vertex>();
+		hash = new HashMap<String, Point3D>();
+		id = 1;
 	}
-	
+
+	public ArrayList<Vertex> getSkeleton() {
+		buildPointArray();
+		addNeighbor();
+
+		return points;
+	}
+
+	public Point3D getPoint(String key) {
+		Point3D p = hash.get(key);
+		return p;
+	}
+
 	private void buildPointArray() {
-		points.add(source.getPoint());
+		points.add(source);
 		for(Blocks b : blockArr) {
 			Rectangle r = b.getRect();
-			points.add(new Point3D(r.getMaxX(),r.getMaxY()));
-			points.add(new Point3D(r.getMaxX(),r.getMinY()));
-			points.add(new Point3D(r.getMinX(),r.getMinY()));
-			points.add(new Point3D(r.getMinX(),r.getMaxY()));
+
+			points.add(createVrtex(r.getMaxX(),r.getMaxY()));
+			points.add(createVrtex(r.getMaxX(),r.getMinY()));
+			points.add(createVrtex(r.getMinX(),r.getMinY()));
+			points.add(createVrtex(r.getMinX(),r.getMaxY()));
 		}
-		points.add(target.getPoint());
+		target.setName(points.size());
+		points.add(target);
 	}
-	
+
+	private Vertex createVrtex(double x,double y) {
+		Point3D p = new Point3D(x,y);
+		hash.put(""+id, p);
+		return new Vertex(p,id++);
+	}
+
 	private boolean intersect(Point3D p,Point3D p2) {
 		boolean flag=true;
 		for(Blocks b : blockArr) {
@@ -40,7 +66,22 @@ public class CalcNeighbor {
 			if(!flag) return false;
 		}
 		return true;
+	}
 
+	private void addNeighbor() {
+		Queue<Vertex> queue = new LinkedList<Vertex>();
+		queue.add(source);
+		boolean visted[] = new boolean[points.size()];
+		while(!queue.isEmpty()) {
+			Vertex current = queue.poll();
+			visted[current.getName()] = true;
+			for(Vertex v : points) {
+				if(!visted[v.getName()] && intersect(current.getPoint(), v.getPoint())) {
+					current.addNeighbor(""+v.getName());
+				}
+			}
+
+		}
 	}
 
 }
--- Task4/src/AutoAlgo/GraphBuilder.java
+++ Task4/src/AutoAlgo/GraphBuilder.java
@@ -1,8 +1,10 @@
 package AutoAlgo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import Gameboard.Blocks;
+import Geom.Point3D;
 import graph.Graph;
 import graph.Node;
 
@@ -10,19 +12,56 @@ public class GraphBuilder {
 	private Vertex source, target;
 	private	ArrayList<Blocks> blockArr;
 	private	Graph G;
+	private ArrayList<Vertex> VetrexList;
+	private CalcNeighbor calc;
 	public GraphBuilder(Vertex source, Vertex target, ArrayList<Blocks> blockArr) {
 		G = new Graph();
 		this.source = source;
 		this.target = target;
 		this.blockArr = blockArr;
+	}
+
+	public Node getGraph() {
 		buildGraph();
+		return G.getNodeByName(""+target);
+
 	}
 
 	private void buildGraph() {
-		G.add(new Node(source.toString()));
-		CalcNeighbor calc = new CalcNeighbor(blockArr, source, target);
-		
+		G.add(new Node(""+source.getName()));
+		calc = new CalcNeighbor(blockArr, source, target);
+		VetrexList = calc.getSkeleton();
+
+		for(Vertex v : VetrexList) {
+			if(!v.isLonely()) {
+				Node node = new Node(""+v.getName());
+				G.add(node);
+			}
+		}
+		G.add(new Node(""+target.getName()));
+		addEdge();
 	}
-	
-	
+
+	private void addEdge() {
+		for(Vertex v : VetrexList) {
+			if(!v.isLonely()) {
+				Iterator<String> it = v.getIterator();	
+				String s;
+				while(it.hasNext()) {
+					s = it.next();
+
+					G.addEdge(""+v.getName(),s ,getDistance(s,v.getPoint()));
+				}
+			}
+		}
+
+	}
+
+	private double getDistance(String s, Point3D p) {
+		Point3D p2 = calc.getPoint(s);
+		return p.distance2D(p2);
+	}
+
+
+
 }
