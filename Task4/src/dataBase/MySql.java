76373b80af2967f46cdbd45b18f31e82adb50123
oravital7 <oravital7@gmail.com>
2019-01-07 14:41:22 +0200

    Update dataBase score

--- Task4/src/dataBase/MySql.java
+++ Task4/src/dataBase/MySql.java
@@ -6,12 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class MySql {
 	private Connection con;
 	private Statement stmt;
 	private ResultSet rs;
+	private HashMap<Integer, String> hash;
 
 	public MySql() {
 		Properties properties = new Properties();
@@ -27,33 +30,74 @@ public class MySql {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		creatKeys();
 
 	}
-	public ArrayList<Object[]> QueryWhereId(String id) {
-		String s = "WHERE FirstID = "+id;
+
+	private void creatKeys() {
+		hash = new HashMap<Integer, String>();
+
+		hash.put(2128259830, "Example 1");
+		hash.put(1149748017, "Example 2");
+		hash.put(-683317070, "Example 3");
+		hash.put(1193961129, "Example 4");
+		hash.put(1577914705, "Example 5");
+		hash.put(-1315066918, "Example 6");
+		hash.put(-1377331871, "Example 7");
+		hash.put(306711633, "Example 8");
+		hash.put(919248096, "Example 9");		
+	}
+	
+	public double[] getScore(boolean ourScore,String id) {
+		double points[] = new double[9];
+		
+		for(Integer map : hash.keySet()) {
+			double point = getScore(map,ourScore,id);
+			String s = hash.get(map);
+			map = Integer.parseInt(""+s.charAt(s.length()-1));
+			points[map-1] = point;
+		}
+		return points;
+	}
+	
+	public ArrayList<Object[]> Query(String id, boolean myScore) {
+		String s = "WHERE FirstID ";
+		if(myScore) s += "= "+id;
+		else s += "!= "+id;
+
 		try {
 			rs = stmt.executeQuery("SELECT * FROM logs "+s);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 		return QueryDatabase();
 	}
 
-	public ArrayList<Object[]> QueryAllNotId(String id) {
-		String s = "WHERE FirstID != "+id;
+
+	private double getScore(int map,boolean ourScore, String id) {
+		double point = 0;
+		String s = "WHERE FirstID ";
+
+		if(ourScore) s += "= 315392852";
+		else s+= "!= 315392852";
+		
 		try {
-			rs = stmt.executeQuery("SELECT * FROM logs "+s);
+			rs = stmt.executeQuery("SELECT MAX(Point) FROM logs "+s+" AND SomeDouble = "+map);
+			rs.next();
+			point = rs.getDouble(1);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return QueryDatabase();
+		return point;
 	}
+
 
 	private ArrayList<Object[]> QueryDatabase()  {
 		ArrayList<Object[]> list = new ArrayList<Object[]>();
 
 		try {
-			while (rs.next()) {				
+			while (rs.next()) {	
 				Object[] o = {rs.getInt("FirstID"),rs.getInt("SecondID"),
 						rs.getInt("ThirdID"),rs.getTimestamp("LogTime"),
 						rs.getDouble("Point"), translateFile(rs.getInt("SomeDouble"))};
@@ -63,32 +107,17 @@ public class MySql {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 		return list;
 	}
 
-	private String translateFile(int x) {
-		switch(x) {
-		case 2128259830 : return "Example 1";
 
-		case 1149748017 : return "Example 2";
-
-		case -683317070 : return "Example 3";
-
-		case 1193961129 : return "Example 4";
-
-		case 1577914705 : return "Example 5";
-
-		case -1315066918 : return "Example 6";
-
-		case -1377331871 : return "Example 7";
-
-		case 306711633 : return "Example 8";
-
-		case 919248096 : return "Example 9";
-		
-		default : return "Unknown";
-		}
+	private String translateFile(int map) {
+		if(hash.containsKey(map)) return hash.get(map);
+		return "Unknown";		
 	}
+
+
 
 	public void closeConnection() {
 		try {
